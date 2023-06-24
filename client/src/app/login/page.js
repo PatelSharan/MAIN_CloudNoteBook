4bd571f9ea6d84674487254a4361002a5c80ab90
Sharan Patel <sharanpatel4@gmail.com>
2023-06-24 16:57:58 +0530

    client setup

--- client/src/app/login/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-    return (
-        <div >Login page hai </div>
-    )
-}
-
-export default page
--- client/src/app/signup/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-    return (
-        <div>SignUp page</div>
-    )
-}
-
-export default page
