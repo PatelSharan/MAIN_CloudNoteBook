2c10dd8cc59591581e3df74c8aa72db3ba4056bd
Sharan Patel <sharanpatel4@gmail.com>
2023-06-26 14:02:40 +0530

    Done Registration

--- client/src/app/login/page.js
+++ client/src/app/login/page.js
@@ -109,38 +109,36 @@ const page = () => {
     }
     return (
         <>
-            <section class="text-gray-600 body-font ">
+            <section class="text-gray-600 body-font px-5 py-12">
                 <form action="" method='POST'>
-                    <div class="container px-5 py-12 flex items-center">
-                        <div class="sm:w-[350px] shadow-md border rounded-lg p-8 flex flex-col m-auto w-full mt-10 md:mt-0">
-                            <h2 class="text-gray-900 text-lg font-medium title-font mb-5">Login</h2>
-                            <div class="relative mb-4">
-                                <label for="email" class="leading-7 text-xs text-gray-600">Email</label>
-                                <input type="email" id="email" value={user.email} name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-sm outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" onChange={handleInputs} />
-                            </div>
-                            <div class="relative mb-4">
-                                <label for="password" class="leading-7 text-xs text-gray-600">Password</label>
-                                <input type={showPassword ? 'text' : 'password'} id="password" value={user.password} name="password" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-sm outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-ou t" onChange={mulEvents} />
-                                {/* If Password has Value then Login */}
-                                {passValue ? <button>
-                                    {/* If ShowPassword the Login */}
-                                    {!showPassword ? <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-eye-fill cursor-pointer absolute right-3 bottom-3" viewBox="0 0 16 16" onClick={() => { { !showPassword ? setShowPassword(true) : setShowPassword(false) } }}>
-                                        <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
-                                        <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
-                                    </svg> : <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-eye-slash-fill cursor-pointer absolute right-3 bottom-3" viewBox="0 0 16 16" onClick={() => { { !showPassword ? setShowPassword(true) : setShowPassword(false) } }}>
-                                        <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.089z" />
-                                        <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12-.708.708z" />
-                                    </svg>}
-                                </button> : <button></button>}
-                            </div>
-                            <span className='text-xs text-end mb-3 text-blue-700 font-semibold hover:underline cursor-pointer'>Forgot Password ?</span>
-                            <button class="text-white bg-slate-700 border-0 py-2 px-8 focus:outline-none hover:bg-slate-800 rounded text-sm" onClick={loginUser}>Login</button>
-                            <p class="text-xs text-gray-500 mt-3">Don't Have An Account ? <Link className='text-blue-700 cursor-pointer hover:underline font-semibold' href={'/signup'}>SignUp</Link></p>
+                    <div class="rounded-lg p-4 flex flex-col m-auto mt-10 md:mt-0 sm:w-[80vw]">
+                        <h2 class="text-gray-900 text-2xl font-medium title-font mb-5">Login</h2>
+                        <div class="relative mb-4">
+                            <label for="email" class="leading-7 text-xs text-gray-600">Email</label>
+                            <input type="email" id="email" value={user.email} name="email" class="w-full bg-white border-b-2 border-black focus:border-b-blue-700  text-sm outline-none text-gray-700 px-3 leading-8 transition-colors duration-200 ease-in-out mb-3" onChange={handleInputs} />
                         </div>
+                        <div class="relative mb-4">
+                            <label for="password" class="leading-7 text-xs text-gray-600">Password</label>
+                            <input type={showPassword ? 'text' : 'password'} id="password" value={user.password} name="password" class="w-full bg-white border-b-2 border-black focus:border-b-blue-700  text-sm outline-none text-gray-700 px-3 leading-8 transition-colors duration-200 ease-in-out mb-3" onChange={mulEvents} />
+                            {/* If Password has Value then Login */}
+                            {passValue ? <button>
+                                {/* If ShowPassword the Login */}
+                                {!showPassword ? <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-eye-fill cursor-pointer absolute right-3 bottom-3" viewBox="0 0 16 16" onClick={() => { { !showPassword ? setShowPassword(true) : setShowPassword(false) } }}>
+                                    <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+                                    <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
+                                </svg> : <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-eye-slash-fill cursor-pointer absolute right-3 bottom-3" viewBox="0 0 16 16" onClick={() => { { !showPassword ? setShowPassword(true) : setShowPassword(false) } }}>
+                                    <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.089z" />
+                                    <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12-.708.708z" />
+                                </svg>}
+                            </button> : <button></button>}
+                        </div>
+                        <span className='text-xs text-end mb-3 text-blue-700 font-semibold hover:underline cursor-pointer'>Forgot Password ?</span>
+                        <button class="text-white bg-slate-800 border-0 py-2 px-8 focus:outline-none hover:bg-slate-700 rounded text-sm w-36" onClick={loginUser}>Login</button>
+                        <p class="text-xs text-gray-500 mt-3">Don't Have An Account ? <Link className='text-blue-700 cursor-pointer hover:underline font-semibold' href={'/signup'}>SignUp</Link></p>
                     </div>
-                </form>
+                </form >
                 <ToastContainer />
-            </section>
+            </section >
         </>
     )
 }
