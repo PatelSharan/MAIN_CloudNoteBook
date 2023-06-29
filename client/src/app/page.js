5b6599896cd312345cda64480e08efcce84ef07a
Sharan Patel <sharanpatel4@gmail.com>
2023-06-29 14:45:56 +0530

    changeing backEndurl

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -6,7 +6,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 export default function Home() {
 
-  const backEndurl = 'https://cloudnotebook.onrender.com'
+  const backEndurl = 'http://localhost:7000'
 
   const [note, setNote] = useState({
     title: '', body: ''
