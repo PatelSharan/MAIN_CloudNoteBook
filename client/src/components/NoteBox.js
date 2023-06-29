11c001f96a8fd6c1ab49a27e9a1d30d1d018678f
Sharan Patel <sharanpatel4@gmail.com>
2023-06-29 13:51:07 +0530

    try

--- client/src/components/NoteBox.js
+++ client/src/components/NoteBox.js
@@ -59,10 +59,9 @@ const NoteBox = () => {
     return (
         <>
             {notes.map((note) => {
-                // const { title, body } = notes
                 return (
-                    < section class="text-gray-600 body-font" >
-                        <div class="flex flex-wrap px-5 py-3 m-auto  sm:w-[80vw]" key={note._id}>
+                    < section class="text-gray-600 body-font" key={note._id} >
+                        <div class="flex flex-wrap px-5 py-3 m-auto  sm:w-[80vw]">
                             <div class="flex border shadow-md rounded border-gray-200 p-5 sm:flex-row flex-col w-full">
                                 <div class="flex-grow">
                                     <h2 class="text-gray-900 text-lg title-font font-medium mb-3">{note.title}</h2>
