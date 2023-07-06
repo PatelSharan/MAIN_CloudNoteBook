f91f1e16f517ca7d2a219df3db234d4fb595b428
Sharan Patel <sharanpatel4@gmail.com>
2023-07-06 15:23:10 +0530

    MAIN - Set Navbar menu icon size and add comment to identify svgs

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -16,7 +16,8 @@ const Navbar = () => {
 
                 {/* To open Navbar */}
                 <div className='mb-3 absolute left-2 top-4 cursor-pointer' onClick={() => { setShowNavbar(true) }}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+                    {/* Menu Svg */}
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
                         <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
                     </svg>
                 </div>
@@ -26,6 +27,7 @@ const Navbar = () => {
                     <div className='fixed top-0 left-0 z-10'>
                         <div className='w-64 h-[100vh] bg-black shadow-md flex flex-col items-center p-5'>
                             <div className='absolute top-4 left-4 sm:right-6'>
+                                {/* Close Svg */}
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-x-circle-fill cursor-pointer" viewBox="0 0 16 16" onClick={() => { setShowNavbar(false) }}>
                                     <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
                                 </svg>
@@ -75,8 +77,9 @@ const Navbar = () => {
                 </>
                     : (
                         // If User login then show user Profile
-                        <div className='mb-3 absolute right-5 top-3 cursor-pointer' onClick={() => { loginContext.setShowProfile(true) }}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
+                        <div className='mb-3 absolute right-4 top-4 cursor-pointer' onClick={() => { loginContext.setShowProfile(true) }}>
+                            {/* User Svg */}
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
                                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                                 <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
                             </svg>
@@ -89,6 +92,7 @@ const Navbar = () => {
                         <div className='absolute top-0 right-0'>
                             <div className='w-64 sm:w-80 h-[100vh] bg-gray-100 shadow-md border flex flex-col items-center p-5'>
                                 <div className='absolute top-3 right-3 sm:right-6'>
+                                    {/* User Svg */}
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-x-circle-fill text-black cursor-pointer" viewBox="0 0 16 16" onClick={() => { loginContext.setShowProfile(false) }}>
                                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
                                     </svg>
