c5fa742af10542ea9ef5926cee05b5400485393a
Sharan Patel <sharanpatel4@gmail.com>
2023-07-07 17:36:18 +0530

    try2

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -3,7 +3,7 @@ import { useContext, useState } from "react"
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Router, useRouter } from 'next/navigation';
-import LoginContext from "@/contexts/login/LoginContext";
+import LoginContext from "@/contexts/login/LoginContex";
 import Link from "next/link";
 
 
